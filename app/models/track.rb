04740986961993822bd22fcc2b60fdb82cbc90b3
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-05-28 11:37:59 -0500

    Added validation for name and distance. Passing tests for distance and name.

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,18 @@
 class Track < ActiveRecord::Base
+  validates :name, presence: true,
+                   length: {minimum: 3,
+                            too_short: "name should have atleast 3 characters"
+                           }
+  validates :distance, presence: true,
+                       numericality: {
+                         greater_than_or_equal_to: 0.1
+                       }
+  before_validation :round_distance, on: :create
+
+  protected
+  def round_distance
+    unless self.distance.nil?
+      self.distance = self.distance.round(1)
+    end
+  end
 end
--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -2,11 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Track, type: :model do
   describe "validations" do
-    it "should be invalid without a name"
-    it "should be invalid if name is all whitespace"
-    it "should be invalid if name is less than 1 chars"
-    it "should be invalid without a distance"
+    it "should be invalid without a name" do
+      described_class.new(name: nil, distance: 0.5).should_not be_valid
+    end
+    it "should be invalid if name is all whitespace" do
+      described_class.new(name: "         ", distance: 0.5).should_not be_valid
+    end
+    it "should be invalid if name is <= 2 chars" do
+      described_class.new(name: "tu", distance: 0.5).should_not be_valid
+    end
+    it "should be invalid without a distance" do
+      described_class.new(name: "Road Way 5", distance: nil).should_not be_valid
+    end
     it "should have unique names"
+    it "should round distances to the nearest tenth" do
+      track1 = described_class.new(name: "Road Way 5", distance: 0.26)
+      track1.save
+      track1.distance.should eq(0.3)
+    end
+    it "should be invalid if distance is < 0.1" do
+      described_class.new(name: "Road Way 5", distance: -0.5).should_not be_valid
+    end
   end
   it "should have a factory"
 end
