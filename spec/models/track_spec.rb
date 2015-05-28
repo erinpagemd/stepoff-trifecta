c1e879101f08c5b9a2e29c59b6604b2ae7020372
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-05-28 09:58:13 -0500

    stubs for validations tests for Track model

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Track, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "should be invalid without a name"
+    it "should be invalid if name is all whitespace"
+    it "should be invalid if name is less than 1 chars"
+    it "should be invalid without a distance"
+    it "should have unique names"
+  end
+  it "should have a factory"
 end
