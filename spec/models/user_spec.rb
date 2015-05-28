8afd65a14ed68ee2503a967141654c756a4dd275
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-05-28 15:43:54 -0500

    Added tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "should be invalid if name is nil"
+    it "should be invalid if name too short"
+    it "should be invalid if email is nil"
+    it "should be invalid if email is not formatted correctly"
+    it "should have a working factory"
+    it "should have a secure password"
+    it "should be invalid if password is empty"
+  end
 end
