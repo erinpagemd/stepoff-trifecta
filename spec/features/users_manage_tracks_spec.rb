66db7344ec97e95328583b954e482da199260ecd
Eliza Brock <eliza@elizabrocksoftware.com>
2015-06-01 14:17:14 -0500

    Expand the test.

--- spec/features/users_manage_tracks_spec.rb
+++ spec/features/users_manage_tracks_spec.rb
@@ -5,12 +5,24 @@ feature "Users manage tracks" do
   end
 
   scenario "viewing list of tracks" do
-    Fabricate(:track, name: "Bathroom", distance: 0.1)
-    Fabricate(:track, name: "Breakroom", distance: 0.15)
+    Fabricate(:track, name: "Bathroom", distance: 0.15)
     Fabricate(:track, name: "Around the building", distance: 0.2)
+    Fabricate(:track, name: "Breakroom", distance: 0.1)
     visit root_path
     click_on "Walking Tracks"
     current_path.should == "/tracks" # We wouldn't normally test the exact path
+    within("ul#walking_tracks") do
+      page.should have_css("li:nth-child(1)", text: "Around the building")
+      page.should have_css("li:nth-child(2)", text: "Bathroom")
+      page.should have_css("li:nth-child(3)", text: "Breakroom")
+    end
+    click_on "Sort: Length"
+    within("ul#walking_tracks") do
+      page.should have_css("li:nth-child(1)", text: "Breakroom")
+      page.should have_css("li:nth-child(2)", text: "Bathroom")
+      page.should have_css("li:nth-child(3)", text: "Around the building")
+    end
+    click_on "Sort: Alphabetical"
     within("ul#walking_tracks") do
       page.should have_css("li:nth-child(1)", text: "Around the building")
       page.should have_css("li:nth-child(2)", text: "Bathroom")
